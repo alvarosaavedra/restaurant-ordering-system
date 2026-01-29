6e193549b84aaa015f6306823f4910b7e4eaa481
Alvaro Saavedra <asaabe@gmail.com>
2026-01-28 22:37:16 -0300

    fix(toast): increase z-index to render above navigation

--- src/lib/components/ToastContainer.svelte
+++ src/lib/components/ToastContainer.svelte
@@ -3,7 +3,7 @@
 	import { toastStore, remove } from '$lib/stores/toast';
 </script>
 
-<div class="fixed top-4 right-4 z-50 flex flex-col gap-2 max-w-sm w-full">
+ <div class="fixed top-4 right-4 z-[130] flex flex-col gap-2 max-w-sm w-full">
 	{#each $toastStore as toast (toast.id)}
 		<Toast
 			id={toast.id}
